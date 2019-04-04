7785b3b7098c831fbffe8165ecc02e52bea6bfc1
Timothy Alban <timothyalban@trentu.ca>
2019-04-04 14:10:23 -0400

    added code to the board

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,20 @@ import javax.swing.*;
 
 public class Board extends JPanel{
 
+    Character k; // for knight
+    Image i;
+    Timer time;
 
+    public Board() {
+
+        k = new Character();
+        setFocusable(TRUE);
+        ImageIcon bg = new ImageIcon(H:\Winter 2019\Cois 2240\Final Project\freetileset\png\BG);
+        i =  bg.getImage();
+        time = new Timer(5, this);
+        time.start();
+
+    }
 
 
 }
