7caa3d3a8fdb84575eb6ce1b71632ee7471d3565
Tim Alban <timothyalban@trentu.ca>
2019-04-03 22:31:49 -0400

    simplified the GameWindow class by removing start/stop methods and seperating the frame and the board

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,54 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GameWindow extends Canvas implements Runnable {
+public class GameWindow extends Canvas{
 
     public static int WIDTH = 640;
     public static int HEIGHT = WIDTH;
     public static String TITLE = "ZeldaKnockOff 2";
-    public static GameWindow zeldish = new GameWindow();
-
-    public boolean isRunning = false;
-    public Thread startNStop = new Thread();
 
 
-
-    public void start(){
-        if (isRunning)
-            return;
-        else
-            isRunning = true;
-        startNStop = new Thread(this);
-        startNStop.start();
-
-    }
-    public void stop(){
-        if(!isRunning)
-            return;
-        else
-            isRunning = false;
-
-        System.exit(1);
-    }
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame(TITLE);
-        frame.add(zeldish);
+        frame.add();
         frame.setSize(WIDTH,HEIGHT);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setFocusable(true);
-        frame.setBackground(Color.BLACK);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
-        zeldish.start();
     }
 
-    @Override
-    public void run() {
 
-
-
-    }
 }
