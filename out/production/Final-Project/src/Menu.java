1faa3f1ccb4c8d8cbb9803c29c4da4e57c54f3a9
Tim Alban <timothyalban@trentu.ca>
2019-04-05 23:26:29 -0400

    final commit blocked out buttons

--- out/production/Final-Project/src/Menu.java
+++ out/production/Final-Project/src/Menu.java
@@ -1,28 +1,79 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferStrategy;
+//implements ActionListener
+public class Menu implements ActionListener {
 
-public class Menu implements ActionListener{
+    private static Rectangle play, tutorial, highScores;
 
-    private static Rectangle play, about, highscores;
-    public static GameWindow win = new GameWindow();
-    public static int centerX = win.getWidth()/2;
-    public static int centerY = win.getHeight()/2;
+    private static GameWindow win = new GameWindow();
+    private static int centerX = win.getWidth()/2;
+    private static int centerY = win.getHeight()/2;
 
-    public static Renderer render = new Renderer();
+    private static Renderer render = new Renderer();
 
-    public static final String SPRITE_DIR = "./png/";
+    private static final String SPRITE_DIR = "./png/";
 
 
+    //This was the button declaration but it broke the program so I had to comment it out
+/*
+    private static JButton playBtn = new JButton(){
+        {
+            playBtn.setSize(350,60);
+            playBtn.setLocation(centerX -200, centerY - 60);
+            playBtn.setOpaque(false);
+            playBtn.setContentAreaFilled(false);
+            playBtn.setBorderPainted(false);
+            playBtn.setEnabled(true);
+
+        }
+
+    };
+    private static JButton tutBtn = new JButton(){
+        {
+            tutBtn.setSize(350,60);
+            tutBtn.setLocation(centerX -200, centerY +10);
+            tutBtn.setOpaque(false);
+            tutBtn.setContentAreaFilled(false);
+            tutBtn.setBorderPainted(false);
+            tutBtn.setEnabled(true);
+        }
+
+    };
+
+    private static JButton hBtn = new JButton(){
+        {
+            hBtn.setSize(350,60);
+            hBtn.setLocation(centerX -200, centerY + 80);
+            hBtn.setOpaque(false);
+            hBtn.setContentAreaFilled(false);
+            hBtn.setBorderPainted(false);
+            hBtn.setEnabled(true);
+        }
+
+    };
+
+
+    public static JButton getPlayBtn() {
+        return playBtn;
+    }
+    public static JButton getTutBtn(){
+        return tutBtn;
+    }
+    public static JButton getHBtn(){
+        return hBtn;
+    }
+*/
+//creates the rectangles which were mostly made just to be able to see where to put the buttons
 
     public static void create(){
-
-        play = new Rectangle(centerX -200, centerY - 60, 350, 60);
-        about = new Rectangle(centerX - 200,centerY +10, 350 , 60);
-        highscores = new Rectangle(centerX - 200,centerY +80 , 350, 60);
+         play = new Rectangle(centerX -200, centerY - 60, 350, 60);
+         tutorial = new Rectangle(centerX - 200,centerY +10, 350 , 60);
+         highScores = new Rectangle(centerX - 200,centerY +80 , 350, 60);
     }
-
+    //creates the outline of a rectangle
     public static void drawButton(Graphics g, Rectangle r){
 
         g.setColor(Color.WHITE);
@@ -30,12 +81,12 @@ public class Menu implements ActionListener{
 
     }
 
-
+    //applies draw button to all the rectangles
     public static void drawMenu(Graphics g){
         g.drawImage(render.imgCreate(SPRITE_DIR + "HomeScreen.png"),0,0,null);
         drawButton(g, play);
-        drawButton(g,about);
-        drawButton(g,highscores);
+        drawButton(g,tutorial);
+        drawButton(g,highScores);
     }
 
     @Override
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,145 +3,174 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.Timer;
+
 import javax.swing.JFrame;
 
-    public class GameWindow extends Canvas implements ActionListener, Runnable {
 
-            public final static int WIDTH = 640;
-            public final static int HEIGHT = 480;
-            public final static String TITLE = "Zeldish";
-            public static final String SPRITE_DIR = "./png/";
+public class GameWindow extends Canvas implements Runnable {
 
-            public boolean running = false;
-            public Thread main2;
-            private Renderer render;
+    public final static int WIDTH = 640;
+    public final static int HEIGHT = 480;
+    public final static String TITLE = "Zeldish";
+    public static final String SPRITE_DIR = "./png/";
+
+    public boolean running = false;
+    public Thread main2;
+    private Renderer render;
+
+    public static GameWindow window = new GameWindow();
 
 
-            public static GameWindow window = new GameWindow();
+    //creates a method that needs to execute before another one can start
+    // synchronized just means it has to finish before anything else when it starts.
+    private synchronized void start() {
+        if (running)
+            return;
+        else
+            running = true;
+        main2 = new Thread(this);
+        main2.start();
+    }
+    //stops the thread when the game is stopped
+    private synchronized void stop() {
+        if (!running) {
+            return;
+        } else
+            running = false;
+
+        try {
+            main2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+    }
+    //starts the processes
+// starts creating the squares in menu and initializes render.
+    public void init() {
+        Menu.create();
+        render = new Renderer();
+    }
 
 
-            //creates a method that needs to execute before another one can start
-            private synchronized void start() {
-                if (running)
-                    return;
-                else
-                    running = true;
-                main2 = new Thread(this);
-                main2.start();
+//essentially just starts everything.
+    // get init() going and  also scene(), I was going to add more timing stuff and maybe fps but ran out of time
+    @Override
+    public void run() {
+        init();
+        long lastTime = System.nanoTime();
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+
+        while (running) {
+            long currentTime = System.nanoTime();
+            delta += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                delta--;
             }
-
-            private synchronized void stop() {
-                if (!running) {
-                    return;
-                } else
-                    running = false;
-
-                try {
-                    main2.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.exit(1);
-            }
-
-            public void init() {
-                Menu.create();
-                render = new Renderer();
-            }
-
-            public void ticks() {
-            }
-
-            @Override
-            public void run() {
-                init();
-                long lastTime = System.nanoTime();
-                double numTicks = 60;
-                double delta = 0;
-                double n = 1000000000 / numTicks;
-                int frames = 0;
-                int ticks = 0;
-                long timer = System.currentTimeMillis();
-
-                while (running) {
-                    long currentTime = System.nanoTime();
-                    delta += (currentTime - lastTime);
-                    lastTime = currentTime;
-
-                    if(delta >= 1) {
-                    ticks ++;
-                    delta--;
-                    }
-                    scene();
-                    frames++;
+            scene();
 
 
-                }
-            }
+
+        }
+    }
 
 
-            @Override
+//creates the background
+    public void background(Graphics g){
+        Menu menu = new Menu();
+        menu.create();
+        menu.drawMenu(g);
+
+
+
+        //I needed the button declartion to work for this to work
+        // this would be used to switch from the menu to the game/tutorial/highscores
+/*
+        menu.getPlayBtn().addActionListener(new ActionListener(){
+
             public void actionPerformed(ActionEvent e){
-
+                g.drawImage(render.imgCreate(SPRITE_DIR + "BG/BG.png"),-50,-300,null);
             }
-            public void background(Graphics g){
-
-            Menu menu = new Menu();
-            menu.create();
-            menu.drawMenu(g);
-
-            //g.drawImage(render.imgCreate(SPRITE_DIR + "BG/BG.png"),-50,-300,null);
+        });
 
 
+        menu.getTutBtn().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                {
+                    JOptionPane.showMessageDialog(null,"Heh, yeah no");
+                }
             }
-            public int getWidth(){
-                return WIDTH;
+        });
+
+
+        menu.getHBtn().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                {
+                    JOptionPane.showMessageDialog(null,"Heh, yeah no");
+                }
             }
-            public int getHeight(){
-                return HEIGHT;
-            }
+        });
 
-
-
-            public void foreground(Graphics g){
-
-            }
-
-
-            public void scene(){
-                BufferStrategy bs = this.getBufferStrategy();
-                    if(bs == null){
-                        createBufferStrategy(3);
-                        return;
-                    }
-
-                    Graphics g = bs.getDrawGraphics();
-                    g.setColor(Color.BLACK);
-                    g.fillRect(0,0,WIDTH,HEIGHT);
-                    ///////////////////////
-                    background(g);
-                    g.dispose();
-                    bs.show();
-            }
-
-
-            public static void main(String[]args){
-
-                JFrame frame = new JFrame(TITLE);
-                frame.add(window);
-                frame.setSize(WIDTH,HEIGHT);
-                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                frame.setFocusable(true);
-                frame.setLocationRelativeTo(null);
-                frame.setResizable(false);
-                frame.setVisible(true);
-                frame.pack();
-                window.start();
-            }
-
-
+*/
 
     }
+
+    //couple getters
+    public int getWidth(){
+        return WIDTH;
+    }
+    public int getHeight(){
+        return HEIGHT;
+    }
+
+
+    //unimplemented method that was going to be used for the tiles
+    public void foreground(Graphics g){
+
+    }
+
+//uses the Bufferstrategy class from canvas to render.
+    public void scene(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,WIDTH,HEIGHT);
+        ///////////////////////
+        background(g);
+        g.dispose();
+        bs.show();
+    }
+
+// creates the JFrame and also makes sure that it is an appropriate size, etc
+// .
+    public static void main(String[]args){
+
+        JFrame frame = new JFrame(TITLE);
+        frame.add(window);
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setFocusable(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.pack();
+        window.start();
+    }
+
+
+
+}
+
